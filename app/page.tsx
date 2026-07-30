71d7dc68fc4feeb8c14d89e9e6f4a552ae3ae11b
I-Maged <magedhanafy@gmail.com>
2026-07-30 07:50:14 +0300

    tailwind & globals css

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const page = () => {
-  return <h1>JobApp</h1>;
+  return <h1 className="bg-accent-dark text-text-muted  text-3xl ">JobApp</h1>;
 };
 
 export default page;
